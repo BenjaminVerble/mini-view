1f368d0549021c078476ec1a17df878d3a7fbdd0
Ben Verble <ben.verble@nike.com>
2015-05-05 09:21:05 -0700

    set up repo

---
+++ examples/list-items/app.js
@@ -0,0 +1,72 @@
+var createView = require('../../index');
+
+var people = [
+    {name: 'Bart'},
+    {name: 'Homer'},
+    {name: 'Lisa'},
+    {name: 'Marge'},
+    {name: 'Maggie'}
+];
+
+function createListItem (person) {
+    var listItemTpl = `<li>{{name}}</li>`;
+
+    var $listItemEl;
+
+    return createView({
+        template: listItemTpl,
+        context: person,
+        init: function (v) {
+
+        }
+    });
+}
+
+function createList () {
+    var listTpl = `<ul></ul>`;
+
+    var $listEl;
+
+    return createView({
+        template: listTpl,
+        init: function (v) {
+            people.forEach(function(person){
+                v.$el.appendChild(createListItem(person).$el);
+            })
+            window.$listEl = v.$el;
+            v.$el.addEventListener('click', function(event) {
+                var clickedEl = event.target;
+                clickedEl.style.color = 'blue';
+            });
+        }
+    });
+}
+
+function createMain () {
+
+    var mainTpl = `
+        <div>
+            <h2>This is Main</h2>
+            <p>{{name}}</p>
+        </div>
+    `;
+
+    var $mainEl;
+    var $header;
+
+    return createView({
+        template: mainTpl,
+        context: {name: 'Ben'},
+        publicFunc: function () {},
+        init: function (v) {
+            $header = v.$el.querySelector('h2');
+            v.$el.appendChild(createList().$el);
+        }
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var $main = document.getElementById('main');
+    $main.appendChild(createMain().$el);
+});
---
+++ index.js
@@ -0,0 +1,86 @@
+/**
+ * http://www.2ality.com/2012/08/underscore-extend.html
+ * Axel Rauschmayer
+**/
+
+function update(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        Object.getOwnPropertyNames(source).forEach(function(propName) {
+            Object.defineProperty(target, propName,
+                Object.getOwnPropertyDescriptor(source, propName));
+        });
+    });
+    return target;
+};
+/**
+ * http://www.2ality.com/2012/08/underscore-extend.html
+ * Axel Rauschmayer
+**/
+function clone(obj) {
+    var copy = Object.create(Object.getPrototypeOf(obj));
+    update(copy, obj);
+    return copy;
+}
+
+/**
+ * https://github.com/Leaflet/Leaflet/blob/master/src/core/Util.js#L108-L118
+ *
+**/
+
+function interpolate(str, data) {
+    return str.replace(/\{{ *([\w_]+) *\}}/g, function (str, key) {
+        var value = data[key];
+        if (value === undefined) {
+            throw new Error('No value provided for variable ' + str);
+        } else if (typeof value === 'function') {
+            value = value(data);
+        }
+        return value;
+    });
+}
+
+function createView(opts) {
+
+    var view = clone(opts);
+
+    if (!opts || !opts.template) {
+        throw new Error('createView: an options object with a template property (string) must be provided when calling createView');
+    }
+
+    var templateFunc;
+
+    if (!templateFunc) {
+        templateFunc = interpolate.bind(undefined, opts.template);
+        createView.cache[opts.template] = templateFunc;
+    }
+
+    var $tempEl = document.createElement('div');
+
+    $tempEl.innerHTML = templateFunc(opts.context);
+
+    view.$el = $tempEl.children[0];
+
+    if (opts.init) {
+        opts.init(view);
+    }
+    return view;
+}
+
+createView.cache = {};
+
+/*
+ * https://github.com/umdjs/umd/blob/master/returnExports.js
+ */
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
+  }
+}(this, function () {
+    return createView;
+}));
